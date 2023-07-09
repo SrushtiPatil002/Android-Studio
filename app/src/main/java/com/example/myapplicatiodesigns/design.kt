616c026f2ef6ab898926a6bf8b592a0e0038e583
SrushtiPatil002 <srushtipatil449@gmail.com>
2023-07-09 19:03:07 +0530

    final designs

---
+++ app/src/main/java/com/example/myapplicatiodesigns/design.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicatiodesigns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class design : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design)
+    }
+}
